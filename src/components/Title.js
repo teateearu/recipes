8573179de9d64dfe79d580dd21ba99bc785da180
“Tea <teateearu@gmail.com>
2017-11-28 11:58:50 +0100

    Testing

---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+class Title extends PureComponent {
+  static propTypes = { // eslint-disable-line no-undef
+    content: PropTypes.string.isRequired,
+  }
+  render() {
+    return (
+      <h1>{ this.props.content }</h1>
+    )
+  }
+}
+export default Title
